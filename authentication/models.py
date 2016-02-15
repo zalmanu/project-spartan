d8d7f3e5bd1b33672730f8ad630d66ddc15d4779
MalesSebastian <malessebastian@gmail.com>
2016-02-15 23:37:10 +0000

    Account model

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,11 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Account(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    city = models.CharField(max_length=40)
+    gender = models.CharField(max_length=10)
+    photo = models.ImageField(null=True)
+    descriere = models.TextField(max_length=300, null=True)
