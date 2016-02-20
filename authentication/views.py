021b49e6742f9b516053fe642001c13aee8bd8be
MalesSebastian <malessebastian@gmail.com>
2016-02-20 18:55:49 +0000

    Revert "Login/Register basic views"
    This reverts commit ed24246fabb56299f5fc2b713d2afbfc4968d20a.

--- authentication/views.py
+++ authentication/views.py
@@ -1,33 +1,9 @@
 from django.shortcuts import render
-from authentication.models import Account
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
 
 
 def register_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        city = request.POST.get('city')
-        country = request.POST.get('country')
-#        phone = request.POST.get('phone')
-        new_user = User.objects.create_user(username, email, password)
-        new_user.save()
-        account = Account.objects.create(user=new_user, city=city, country=country)
-        account.save()
-        return render(request, "authentication/random.html")
-    else:
-        return render(request, "authentication/register.html")
+    return render(request, "authentication/register.html")
 
 
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(request, "authentication/random.html")
     return render(request, "authentication/logIn.html")
