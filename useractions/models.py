94e43a1b20d76833fd52c395a09fd5cb90e6af44
Sebastian <malessebastian@gmail.com>
2016-03-06 14:08:08 +0200

    Announcement model added

--- useractions/models.py
+++ useractions/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(null=True, max_length=20)
+    text = models.CharField(null=True, max_length=500)
+    creation_date = models.DateTimeField(editable=False, auto_now_add=True)
+    slug = models.SlugField(default=uuid.uuid1, unique=True)
+    author = models.ForeignKey(User, null=True, blank=True)
+    
+    def get_absolute_url(self):
+        return reverse('announcement', args=[self.slug])
+    
+    class Meta:
+        get_latest_by = 'creation_date'
