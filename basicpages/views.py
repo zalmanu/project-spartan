c91f9ec19c1eebd7ac776b7f3ef23d05b7e0782c
Sebastian <malessebastian@gmail.com>
2016-03-07 16:15:37 +0200

    Posts home listing

--- basicpages/views.py
+++ basicpages/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from useractions.models import Announcement
 
 
 def home(request):
     if request.user.is_authenticated():
         return render(request, 'useractions/home.html')
     else:
-        return render(request, 'basicpages/index.html')
+        return render_to_response('basicpages/index.html', {
+            'ann' : Announcement.objects.all().order_by('-creation_date')[:3]
+            })
