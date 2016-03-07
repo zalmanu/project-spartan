3fc87afb6c1024073c2b9aaf9943946717d313bd
Sebastian <malessebastian@gmail.com>
2016-03-07 16:44:14 +0200

    Adress field

--- useractions/models.py
+++ useractions/models.py
@@ -12,7 +12,8 @@ class Announcement(models.Model):
     creation_date = models.DateTimeField(editable=False, auto_now_add=True)
     slug = models.SlugField(default=uuid.uuid1, unique=True)
     author = models.ForeignKey(User, null=True, blank=True)
-    
+    adress = models.CharField(null=True, max_length=500) 
+
     def get_absolute_url(self):
         return reverse('announcement', args=[self.slug])
     
