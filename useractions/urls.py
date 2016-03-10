8e2319720dbf5fd150c062b3a87be46ebf60a41a
Sebastian <malessebastian@gmail.com>
2016-03-10 08:57:32 +0200

    Create post html added

--- useractions/urls.py
+++ useractions/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^logout/', views.logout_view, name='logout')
+    url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^submit/', views.create_post, name='submit')
 ]
--- useractions/views.py
+++ useractions/views.py
@@ -7,3 +7,6 @@ from django.core.urlresolvers import reverse
 def logout_view(request):
     logout(request)
     return redirect(reverse('home'))
+
+def create_post(request):
+    return render(request, 'useractions/create_post.html')
