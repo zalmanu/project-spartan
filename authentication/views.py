7fbf5e2b3a9874e931b86937e19dcc2378d54026
MalesSebastian <MalesSebastian@gmail.com>
2016-04-04 22:43:13 +0300

    Login/Register accesing

--- authentication/views.py
+++ authentication/views.py
@@ -7,35 +7,41 @@ from django.contrib import messages
 
 
 def register_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        city = request.POST.get('city')
-        country = request.POST.get('country')
-        phone = request.POST.get('phone')
-        photo = request.FILES.get('photo')
-        new_user = User.objects.create_user(username, email, password)
-        new_user.save()
-        account = Account.objects.create(user=new_user, city=city, country=country, telefon=phone, photo=photo)
-        account.save()
-        user = authenticate(username=username, password=password)
-        login(request, user)
+    if request.user.is_authenticated():
         return redirect('/')
     else:
-        return render(request, "authentication/register.html")
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            city = request.POST.get('city')
+            country = request.POST.get('country')
+            phone = request.POST.get('phone')
+            photo = request.FILES.get('photo')
+            new_user = User.objects.create_user(username, email, password)
+            new_user.save()
+            account = Account.objects.create(user=new_user, city=city, country=country, telefon=phone, photo=photo)
+            account.save()
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, "authentication/register.html")
 
 
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect('/')
-        else:
-            messages.error(request, 'Incorrect username or password')
-            return render(request, "authentication/logIn.html")
-    return render(request, "authentication/logIn.html")
+    if request.user.is_authenticated():
+        return redirect('/')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/')
+            else:
+                messages.error(request, 'Incorrect username or password')
+                return render(request, "authentication/logIn.html")
+        return render(request, "authentication/logIn.html")
