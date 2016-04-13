0b70ef1c8e26bfba798855b17ae671d32968162a
Emanuel <covaciemanuel98@gmail.com>
2016-04-13 19:16:12 +0300

    Filtarare anunt dupa o categorie pusa de mine in cod -merge doar pe home

--- basicpages/views.py
+++ basicpages/views.py
@@ -7,8 +7,9 @@ def home(request):
         curruser = request.user
         if request.user.is_active and not  request.user.is_superuser:
             print curruser.account.cod
+            a=Announcement.objects.filter(category='Curatenie')
             return render_to_response('useractions/home.html', {
-            'ann': Announcement.objects.all().order_by('-creation_date')[:5],
+            'ann': a,#Announcement.objects.all().order_by('-creation_date')[:5],
                 'categories': ['Garden', 'Moving', 'Cleaning', 'Babysitting', 'Cooking', 'Others'],
                 'user': curruser,
                 'cod': curruser.account.cod
