1439770747e03bd568dd5d7f30f260a235e7e3fc
Emanuel <covaciemanuel98@gmail.com>
2016-04-22 15:49:39 +0300

    yourPost edit info

--- bidding/views.py
+++ bidding/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-
-
+from django.contrib.auth.models import User
+from authentication.models import Account,Spartan
 
 def myPost(request):
-    return render(request, 'bidding/myPosts.html')
+    return render(request, 'bidding/myPosts.html',)
 
 def post(request):
     return render(request,'bidding/post.html')
 
 def yourPost(request):
-    return render(request,'bidding/yourPost.html')
+     if request.user.is_active and not  request.user.is_superuser:
+            return render(request, 'bidding/yourPost.html', {
+                'cod': request.user.account.cod,
+                })
+     else:
+            return render(request, 'useractions/profile.html',{
+                'cod': '61e1380365703a4c73c2480673d8993b',})
