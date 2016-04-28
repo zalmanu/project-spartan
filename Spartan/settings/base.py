24457d66144bbd9059c6dd756ec2f93e4b7a5a34
Emanuel <covaciemanuel98@gmail.com>
2016-04-28 17:36:59 +0300

    Split settings #52 don't work -manage.py trebuie modificat

--- Spartan/settings/base.py
+++ Spartan/settings/base.py
@@ -2,9 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-SECRET_KEY = 'er&cazxnks3w4jh_6(w5jq10^b05&evags5g%#@r5esleqt%t#'
-
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -29,8 +26,7 @@ INSTALLED_APPS = (
 
 )
 
-RECAPTCHA_PUBLIC_KEY = '6LfOhB4TAAAAAIP6Nw60oYoCdr0OAfDNxkIZa9U7'
-RECAPTCHA_PRIVATE_KEY = '6LfOhB4TAAAAADtMc7XrFyCi5AWP1c8a-AakPs_m'
+
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
@@ -42,14 +38,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-#email settings
-EMAIL_USE_TLS = True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='contactprojectspartan@gmail.com'#insert email
-EMAIL_HOST_PASSWORD='spartan123456789'#insert password
-EMAIL_PORT=587
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 
