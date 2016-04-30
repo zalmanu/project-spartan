29d6ce45c2270032f5c015478911e6df0704420f
Emanuel <covaciemanuel98@gmail.com>
2016-04-30 22:12:07 +0300

    review & spartan page

--- SpartanPage/views.py
+++ SpartanPage/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 # Create your views here.
+@login_required
 def user(request):
-    return render(request, 'SpartanPage/user.html')
+    performanta= (request.user.spartan.raiting + request.user.spartan.tasks)/2
+    return render(request,'SpartanPage/user.html' ,{'performance':performanta,
+                                          'reviews':request.user.spartan.reviews})
