41adff49a09af7c00952fb4a15e5a1cf8cc61618
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-05-01 01:59:56 +0300

    Spartan page removed performanta

--- SpartanPage/views.py
+++ SpartanPage/views.py
@@ -4,6 +4,5 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 @login_required
 def user(request):
-    performanta= (request.user.spartan.raiting + request.user.spartan.tasks)/2
-    return render(request,'SpartanPage/user.html' ,{'performance':performanta,
+    return render(request,'SpartanPage/user.html' ,{
                                           'reviews':request.user.spartan.reviews})
--- settings/base.py
+++ settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = (
     'contactUS',
     'review',
     'SpartanPage',
+    'chat'
 
 )
 
