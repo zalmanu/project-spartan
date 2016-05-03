03c77e6bd533597c39c32208a04d6a4894b4e101
Senibelan <malessebastian@gmail.com>
2016-05-03 18:24:14 +0300

    Category clean fix

--- categories/views.py
+++ categories/views.py
@@ -10,17 +10,9 @@ def category(request, kind):
     categories = Category.objects.all()
     page_category = get_object_or_404(Category, name=kind)
     curruser = request.user
-    if request.user.is_active and not request.user.is_superuser:
-        return render(request, 'category/category.html', {
-            'categories': categories,
-            'kind': page_category,
-            'cod': curruser.account.cod,
-            'ann': Announcement.objects.filter(category=page_category, status=False)
-        })
-    else:
-        return render(request, 'category/category.html', {
-            'categories': categories,
-            'kind': page_category,
-            'cod': '61e1380365703a4c73c2480673d8993b',
-            'ann': Announcement.objects.filter(category=page_category, status=False)
-        })
+    return render(request, 'category/category.html', {
+        'categories': categories,
+        'kind': page_category,
+        'cod': curruser.account.cod,
+        'ann': Announcement.objects.filter(category=page_category, status=False)
+    })
