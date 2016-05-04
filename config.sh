fbf236a64ee50c6fa952916c23dc272940e0564e
MalesSebastian <MalesSebastian@gmail.com>
2016-05-04 16:02:33 +0300

    Config updated

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
-sudo apt-get install python-pip 
-pip2 install django==1.9.5
-pip2 install django-widget-tweaks==1.4.1
-pip2 install django-recaptcha==1.0.5
+sudo apt-get update
+sudo apt-get install python-pip python-dev libpq-dev
+sudo pip2 install virtualenv
+mkdir ~/spartan
+cd ~/spartan
+virtualenv spartan
+source spartan/bin/activate
+pip2 install django==1.9.5 django-widget-tweaks==1.4.1 django-recaptcha==1.0.5 gunicorn
+
