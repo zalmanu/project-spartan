a30fb4a85246557aadca00d6e2c2f879f1a66eb2
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-05-05 11:57:48 +0300

    chat aranjat

--- chat/forms.py
+++ chat/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class SendMessageForm(forms.Form):
-    message = forms.CharField(widget=forms.TextInput(attrs={'class': 'textarea' , 
-                                                            'placeholder':'Type here!'}))
+    message = forms.CharField(widget=forms.TextInput(
+                                 attrs={'class': 'textarea' ,
+                                        'placeholder': 'Type here!'}))
