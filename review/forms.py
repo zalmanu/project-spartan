fed0d6e2a5a78c4932dc3d0d283e2e68b42ab4b7
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-05-05 12:14:48 +0300

    review rearanjat

--- review/forms.py
+++ review/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    message = forms.CharField(max_length=1000, label="Review-message", required=False)
+    message = forms.CharField(max_length=1000, label="Review-message",
+                              required=False)
--- review/models.py
+++ review/models.py
@@ -9,6 +9,7 @@ from posts.models import Announcement
 class Review(models.Model):
     receiver = models.ForeignKey(Spartan, related_name='reviews')
     submitter = models.ForeignKey(User, related_name='reviews')
-    message=models.CharField(null=True, max_length=1000)
-    data=models.DateField('Data publicarii review-ului', null=True)
-    creation_date = models.DateTimeField(editable=False, auto_now_add=True, null=True)
+    message = models.CharField(null=True, max_length=1000)
+    data = models.DateField('Data publicarii review-ului', null=True)
+    creation_date = models.DateTimeField(editable=False, auto_now_add=True,
+                                         null=True)
