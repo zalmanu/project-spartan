5f8afd6bc5c6ab14820a5725871c05d1f11e393e
Senibelan <malessebastian@gmail.com>
2016-05-06 17:02:43 +0300

    Error not displaying incomplete

--- profiles/models.py
+++ profiles/models.py
@@ -9,6 +9,15 @@ class User_Edit(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
-        widgets = {
-            'username': Textarea(attrs={'value': model.username })
-        }
+    
+    def clean_email(self):
+        user_email = self.cleaned_data['email']
+        username = self.cleaned_data.get('username')
+        if email User.objects.objects.filter(email=email).exclude(username):
+            raise forms.ValidationError("This email already exists")
+
+    def clean_username(self):
+        user_name = self.cleaned_data['username']
+        if User.objects.filter(username=user_name).exists():
+            raise forms.ValidationError("This username already exists")
+
