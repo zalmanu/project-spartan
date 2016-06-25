dab23c63f6c68c70dfe4e989e28573e8f42e9797
MalesSebastian <malessebastian@gmail.com>
2016-06-25 14:19:38 +0300

    Report view too repetitive -- not finished

--- report/views.py
+++ report/views.py
@@ -7,62 +7,49 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def report(request):
+    errors = []
+    confirms = []
     curruser = request.user
     if request.method == 'POST':
         form = ReportForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             if request.user.username == username:
-                return render(request, 'report/report.html', {
-                    'cod': curruser.account.code,
-                    'form': form,
-                    'errors': ["This username  is your username "]})
-            status = form.cleaned_data['statut']
-            if status == 'Employer':
-                try:
-                    user = User.objects.get(username=username)
-
-                except User.DoesNotExist:
-                    return render(request, 'report/report.html', {
-                        'cod': curruser.account.code,
-                        'form': form,
-                        'errors': ["This employer doesn't exist"]})
+                errors.append("This username  is your username")
             else:
-                try:
-                    user = User.objects.get(username=username)
-                    if not user.account.has_related_object():
-                        return render(request, 'report/report.html', {
-                            'cod': curruser.account.code,
-                            'form': form,
-                            'errors': ["This spartan doesn't exist"]})
-                except User.DoesNotExist:
-                    return render(request, 'report/report.html', {
-                        'cod': curruser.account.code,
-                        'form': form,
-                        'errors': ["This spartan doesn't exist"]})
-            text = form.cleaned_data['text']
-            report = Report.objects.create(username=username,
-                                           status=status,
-                                           text=text,
-                                           author=request.user,
-                                           )
-            report.save()
-            return render(request, 'report/report.html', {
-                'cod': curruser.account.code,
-                'form': form,
-                'confirm': ['The report was sent!']})
+                status = form.cleaned_data['statut']
+                if status == 'Employer':
+                    try:
+                        User.objects.get(username=username)
+                        text = form.cleaned_data['text']
+                        report = Report.objects.create(username=username,
+                                                       status=status,
+                                                       text=text,
+                                                       author=request.user)
+                        report.save()
+                        confirms.append("The report was sent")
+                    except User.DoesNotExist:
+                        errors.append("This employer doesn't exist")
+                else:
+                    try:
+                        user = User.objects.get(username=username)
+                        text = form.cleaned_data['text']
+                        report = Report.objects.create(username=username,
+                                                       status=status,
+                                                       text=text,
+                                                       author=request.user)
+                        report.save()
+                        confirms.append("The report was sent")
+                    except (User.DoesNotExist or
+                            not user.account.has_related_object()):
+                        print user.username
+                        errors.append("This user doesn't exist")
+
         else:
-            return render(request, 'report/report.html', {
-                'cod': curruser.account.code,
-                'form': form,
-                'errors': ['Invalid form']})
-    else:
-        form = ReportForm
-        if request.user.is_active and not request.user.is_superuser:
-            return render(request, 'report/report.html', {
-                'cod': curruser.account.code,
-                'form': form})
-        else:
-            return render(request, 'report/report.html', {
-                'cod': '61e1380365703a4c73c2480673d8993b',
-                'form': form})
+            errors.append("Invalid form")
+    form = ReportForm
+    return render(request, 'report/report.html', {
+        'cod': curruser.account.code,
+        'form': form,
+        'confirm': confirms,
+        'errors': errors})
