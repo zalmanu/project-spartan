19d8e494597a4cd4487e280833e7bc489a188414
malessebastian <malessebastian@gmail.com>
2016-06-25 15:15:26 +0300

    Report view too repetitive --works

--- report/views.py
+++ report/views.py
@@ -14,25 +14,13 @@ def report(request):
         form = ReportForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
-            if request.user.username == username:
-                errors.append("This username  is your username")
-            else:
-                status = form.cleaned_data['statut']
-                if status == 'Employer':
-                    try:
-                        User.objects.get(username=username)
-                        text = form.cleaned_data['text']
-                        report = Report.objects.create(username=username,
-                                                       status=status,
-                                                       text=text,
-                                                       author=request.user)
-                        report.save()
-                        confirms.append("The report was sent")
-                    except User.DoesNotExist:
-                        errors.append("This employer doesn't exist")
+            try:
+                user = User.objects.get(username=username)
+                if request.user.username == username:
+                    errors.append("This username  is your username")
                 else:
-                    try:
-                        user = User.objects.get(username=username)
+                    status = form.cleaned_data['statut']
+                    if status == 'Employer':
                         text = form.cleaned_data['text']
                         report = Report.objects.create(username=username,
                                                        status=status,
@@ -40,10 +28,19 @@ def report(request):
                                                        author=request.user)
                         report.save()
                         confirms.append("The report was sent")
-                    except (User.DoesNotExist or
-                            not user.account.has_related_object()):
-                        print user.username
-                        errors.append("This user doesn't exist")
+                    else:
+                        if user.account.has_related_object():
+                            text = form.cleaned_data['text']
+                            report = Report.objects.create(username=username,
+                                                           status=status,
+                                                           text=text,
+                                                           author=request.user)
+                            report.save()
+                            confirms.append("The report was sent")
+                        else:
+                            errors.append("This user is not a spartan")
+            except User.DoesNotExist:
+                errors.append("This user does not exist")
 
         else:
             errors.append("Invalid form")
