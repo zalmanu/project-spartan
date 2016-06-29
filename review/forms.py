46ea96102dc8163aac8cb581c1d6ac39a9234c67
MalesSebastian <malessebastian@gmail.com>
2016-06-29 14:40:31 +0300

    Review form reworked

--- review/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class ReviewForm(forms.Form):
-    message = forms.CharField(max_length=1000, label="Review message",
-                              required=False)
--- review/models.py
+++ review/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
+import datetime
 
+from django import forms
 from django.db import models
 from authentication.models import User
 from spartans.models import Spartan
@@ -8,12 +10,18 @@ from spartans.models import Spartan
 class Review(models.Model):
     receiver = models.ForeignKey(Spartan, related_name='reviews')
     submitter = models.ForeignKey(User, related_name='reviews')
-    message = models.CharField(null=True, max_length=1000)
-    data = models.DateField('Review publication day', null=True)
-    creation_date = models.DateTimeField(editable=False, auto_now_add=True,
-                                         null=True)
+    message = models.CharField("Review message", null=True, max_length=1000)
+    data = models.DateField('Review publication day', null=True,
+                            default=datetime.datetime.now())
 
 
 class UrlUnique(models.Model):
     un_hash = models.CharField('Url', unique=True, max_length=33)
     expired = models.BooleanField(default=False)
+
+
+class CreateReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ['message']
