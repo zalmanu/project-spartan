a904bdf38a6f0c1f24b5e4374c60a8ea13d7a791
MalesSebastian <malessebastian@gmail.com>
2016-06-29 13:04:17 +0300

    Contact us form reworked

--- contactUS/models.py
+++ contactUS/models.py
@@ -1,4 +1,6 @@
+from django import forms 
 from django.db import models
+from captcha.fields import ReCaptchaField
 
 
 class ContactUS(models.Model):
@@ -7,3 +9,11 @@ class ContactUS(models.Model):
     phone = models.IntegerField(null=True)
     email = models.CharField(max_length=30)
     message = models.CharField(max_length=1000, null=True)
+
+
+class CreateContact(forms.ModelForm):
+    captcha = ReCaptchaField()
+
+    class Meta:
+        model = ContactUS
+        fields = '__all__'
