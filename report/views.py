3b5be2020afd251204b78ab447b3e770f8ec07ea
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-07-01 10:21:28 +0300

    refactoring report

--- report/views.py
+++ report/views.py
@@ -6,15 +6,15 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def report(request):
     confirms = []
-    curruser = request.user
-    form = CreateReportForm(data=request.POST or None, user=curruser)
+    current_user = request.user
+    form = CreateReportForm(data=request.POST or None, user=current_user)
     if request.method == 'POST':
         if form.is_valid():
-            form.instance.author = curruser
+            form.instance.author = current_user
             form.save()
             confirms.append("The report was sent")
     return render(request, 'report/report.html', {
-        'cod': curruser.account.code,
+        'cod': current_user.account.code,
         'form': form,
         'confirm': confirms
     })
