74b622a539a5aa5b47dfa7fb0003e2a8625693c0
MalesSebastian <malessebastian@gmail.com>
2016-07-01 09:18:37 +0300

    Search url added

--- Spartan/urls.py
+++ Spartan/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     url(r'^', include('faq.urls')),
     url(r'^', include('report.urls')),
     url(r'^', include('aboutUS.urls')),
+    url(r'^search/', include('haystack.urls')),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
