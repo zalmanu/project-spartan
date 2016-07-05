8570adc1257fa3f4e7bb0bcf2a11cfa4334be4af
MalesSebastian <malessebastian@gmail.com>
2016-07-05 18:15:40 +0300

    Categories tests refactoring

--- categories/tests.py
+++ categories/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, RequestFactory
+from django.http import Http404
 from django.contrib.auth.models import User
 
 from .models import Category
 from .views import category
+from authentication.models import Account
 
 
 class CategoriesViewsTestCase(TestCase):
@@ -11,6 +13,9 @@ class CategoriesViewsTestCase(TestCase):
         self.user = User.objects.create_user(username="tester",
                                              email="smt@smt.com",
                                              password="top_secret")
+        account = Account.objects.create(city="Timisoara", country="Romania",
+                                         user=self.user)
+        account.save()
         self.category = Category.objects.create(name="TestCategory",
                                                 path_bg="test",
                                                 path_banner="test",
@@ -21,11 +26,11 @@ class CategoriesViewsTestCase(TestCase):
     def test_valid_view(self):
         request = self.factory.get('/category')
         request.user = self.user
-        status = category(request, "TestCategory")
-        self.assertEqual(status, 200)
+        status = category(request, self.category.name)
+        self.assertEqual(status.status_code, 200)
 
     def test_invalid_view(self):
         request = self.factory.get('/category')
         request.user = self.user
-        status = category(request, "Blablabla")
-        self.assertEqual(status, 404)
+        with self.assertRaises(Http404):
+            category(request, "dwada")
