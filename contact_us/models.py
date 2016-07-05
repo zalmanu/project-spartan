7e0d24784c06d6cebde403375c76b31a6a3235ef
MalesSebastian <malessebastian@gmail.com>
2016-07-05 10:39:53 +0300

    "Index page functionality"

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,13 +1,10 @@
 from django import forms
 from django.db import models
 from captcha.fields import ReCaptchaField
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class ContactUs(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    phone = PhoneNumberField(null=True)
+    name = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
     message = models.CharField(max_length=1000, null=True)
 
