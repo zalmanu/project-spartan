45b312416999dfcad29e78c68d320b995b46b23b
MalesSebastian <malessebastian@gmail.com>
2016-07-07 08:55:53 +0300

    Chat everything works

--- realtime/consumers.py
+++ realtime/consumers.py
@@ -4,16 +4,23 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 from channels import Group
+from channels.sessions import channel_session
 
 from chat.models import Room, Message
 
 
+@channel_session
 def ws_add(message):
-    Group("chat").add(message.reply_channel)
+    label = message['path'].strip('/').split('/')
+    room = Room.objects.get(slug=label[1])
+    Group("chat-" + label[1]).add(message.reply_channel)
+    message.channel_session['room'] = room.slug
 
 
+@channel_session
 def ws_message(message):
     data = json.loads(message['text'])
+    label = message.channel_session['room']
     room = get_object_or_404(Room, slug=data['room_slug'])
     user = get_object_or_404(User, username=data['user_name'])
     message = Message.objects.create(room=room, message=data['text'],
@@ -35,8 +42,10 @@ def ws_message(message):
         'code': user.account.code,
         'html': html_txt
     }
-    Group("chat").send({'text': json.dumps(dic)})
+    Group("chat-" + label).send({'text': json.dumps(dic)})
 
 
+@channel_session
 def ws_disconnect(message):
-    Group("chat").discard(message.reply_channel)
+    label = message.channel_session['room']
+    Group("chat-" + label).discard(message.reply_channel)
--- static/js/chat_script.js
+++ static/js/chat_script.js
@@ -1,20 +1,25 @@
-socket = new WebSocket("ws://" + window.location.host + window.location.pathname);
-function sendmessage() {o
-     var field = document.getElementById("textcase");
+ socket = new WebSocket("ws://" + window.location.host + window.location.pathname);
+ function sendmessage() {
+ var field = document.getElementById("textcase");
+ if(field.value == '')return false;
      var username = document.getElementById("user_name");
      var url_path = window.location.href.split('/');
      var len = url_path.length;
      message = {
-         message: field.value,
-         room: url_path[len - 2],
-	 user: username.value
+         text: field.value,
+         room_slug: url_path[len - 2],
+	 user_name: username.value
 	 
      }
-     socket.send(JSON.stringify);
-    return false;
+     socket.send(JSON.stringify(message));
+     return false;
  }
-socket.onmessage = function(message){
-    var data = JSON.parse(message.data);
-    var elem = docuemnt.getElementById("chat-frame");
-    elem.innerHTML = data.message;
-}
+ socket.onmessage = function(message){
+     var data = JSON.parse(message.data);
+     var elem = document.getElementById("mesg");
+     var username = document.getElementById("user_name");
+     var clas ='other';
+     if(username.value == data.submitter)clas='self';
+     $("#mesg").append("<li class=" + clas + ">" + data.html);
+     elem.scrollTop = objDiv.scrollHeight;
+ }
