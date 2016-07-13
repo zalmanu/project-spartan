c936623c4867927cb163de3e35865ace3315c7c5
MalesSebastian <malessebastian@gmail.com>
2016-07-13 10:47:41 +0300

    seen notification

--- notifications/views.py
+++ notifications/views.py
@@ -1,6 +1,5 @@
 import json
 
-from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseForbidden, HttpResponse
 
@@ -13,8 +12,7 @@ def seen(request):
         if request.POST.get('notif'):
             context = {"result": "success"}
             notif_id = request.POST.get("notif")
-            print notif_id
-            notification = Notification.get_object_or_404(pk=notif_id)
+            notification = Notification.objects.get(id=notif_id)
             notification.seen = True
             notification.save()
             return HttpResponse(json.dumps(context),
--- static/js/seen_script.js
+++ static/js/seen_script.js
@@ -4,25 +4,37 @@ function seencount(seenstatus){
 	seennumber++;
     }
 }
+
 function seencheck(){
     var elem = document.getElementById("notifnum");
     var iconcount = document.getElementById("iconcount");
     if(seennumber == 0){
-	elem.innerHTML = "No new notifications";
 	iconcount.remove()
     }
     else{
-	elem.innerHTML = "You have " + seennumber + " new notifications";
 	iconcount.innerHTML = seennumber;
     }
+    elem.innerHTML = "You have " + seennumber + " new notifications";
 }
 
 $(document).ready(function(){
         $("#seen_not").hover(function(event){
-            var notif = $(this).data("notification");;
+            var notif = $(this).data("notification");
             $.post("/seen/", { 'notif': notif }, function(response) {
-                 if (response.result == "success") {
-                     console.log("success");
+                if (response.result == "success") {
+		    var iconcount = document.getElementById("iconcount");
+		    var elem = document.getElementById("seen_not");
+		    var seennum = parseInt(iconcount.innerHTML);
+		    var elem_up = document.getElementById("notifnum");
+		    seennum--;
+		    if(seennum == false)
+			iconcount.remove()
+		    else 
+			iconcount.innerHTML = seennum.toString();	
+		    elem_up.innerHTML = "You have " + seennum + " new notifications";	    
+		    elem.id = "";
+		    elem.style = "";
+		    iconc
                  }
             })
             return true;
