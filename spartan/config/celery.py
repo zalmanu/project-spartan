91bda44dea3975c5070459af68459f3536ef9eed
MalesSebastian <malessebastian@gmail.com>
2016-07-22 14:01:45 +0300

    Celery cofing filde update

--- spartan/config/celery.py
+++ spartan/config/celery.py
@@ -26,7 +26,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
-app = Celery('Spartan')
+app = Celery('config')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
