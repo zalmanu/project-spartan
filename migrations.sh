9ef97381bcd45dbc8ead065d659692b361daf20c
MalesSebastian <malessebastian@gmail.com>
2016-07-22 13:42:24 +0300

    Project layout changed

--- migrations.sh
+++ migrations.sh
@@ -2,7 +2,7 @@
 if [ "$1" = "-r" ]; then
     find . -name \migrations -type d -exec rm -r {} +
 else 
-    python manage.py makemigrations authentication bidding categories chat contact_us posts review spartans report error_pages
-    python manage.py migrate
+    python manage.py makemigrations authentication bidding categories chat contact_us posts review spartans report notifications
+    python spartan/manage.py migrate
 fi
 
