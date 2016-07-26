65f58b22349b5828e68ef95bda48829becaca4c3
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-07-26 12:12:14 +0300

    about,faq  page +  login_required

--- spartan/config/urls.py
+++ spartan/config/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -40,10 +41,13 @@ urlpatterns = [
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+
 urlpatterns += [
-    url(r'^faq/$', TemplateView.as_view(template_name="faq/faq.html")),
-    url(r'^about/$', TemplateView.as_view(
-        template_name="about_us/about_us.html")),
+
+    url(r'^faq/', login_required(TemplateView.as_view(template_name="faq/faq.html"))),
+    url(r'^about/', login_required(TemplateView.as_view(template_name="about_us/about_us.html"))),
+
 ]
 
 
