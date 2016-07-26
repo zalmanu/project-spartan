7ec8ff29992d461bde7d5bed30dfdcb06131b0b5
Vlad Temian <vladtemian@gmail.com>
2016-07-26 15:18:02 +0300

    Working web container.

--- run_celery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd spartan
-# run Celery worker for our project myproject with Celery configuration stored in Celeryconf
-su -m myuser -c "celery worker -A config.celeryconf -Q default -n default@%h"
--- run_web.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd spartan
-# migrate db, so we have the latest db schema
-su -m myuser -c "python manage.py migrate"  
-# start development server on public ip interface, on port 8000
-su -m myuser -c "python manage.py runserver 0.0.0.0:8000"
