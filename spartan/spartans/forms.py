7b06988b2565a119b25d4847ce43b483c365b655
MalesSebastian <malessebastian@gmail.com>
2016-07-27 23:36:17 +0300

    Validate cui

--- spartan/spartans/forms.py
+++ spartan/spartans/forms.py
@@ -43,3 +43,33 @@ class CreateSpartanForm(forms.ModelForm):
         cat_name = self.cleaned_data['category']
         category = get_object_or_404(Category, name=cat_name)
         return category
+
+    def clean_cui(self):
+        cui = self.cleaned_data['cui']
+        valid = True
+        if(cui[:2] != "RO"):
+            valid = False
+        else:
+            cui = cui[2:]
+            if not cui.isdigit():
+                valid = False
+            else:
+                testing = 753217532
+                sum = 0
+                if len(cui) > 10 or len(cui) < 6:
+                    valid = False
+                cui = int(cui)
+                c1 = cui % 10
+                cui = int(cui / 10)
+                while(cui > 0):
+                    sum += cui % 10 * testing % 10
+                    cui = cui / 10
+                    testing = testing / 10
+                c2 = sum * 10 % 11
+                if(c2 == 10):
+                    c2 = 0
+                if(c1 == c2):
+                    valid = False
+        if(not valid):
+            raise forms.ValidationError("Enter a valid CUI")
+        return cui
