647b6858ecf4d81587edd8a3c64e2d932d62fa7a
MalesSebastian <malessebastian@gmail.com>
2016-07-30 14:48:16 +0300

    Bids sorted by price

--- spartan/posts/views.py
+++ spartan/posts/views.py
@@ -86,6 +86,7 @@ def post(request, slug):
        request.user != post.spartan.user:
         raise Http404()
     confirms = []
+    bids = post.offers.all().order_by('-price')
     if request.method == 'POST':
         if request.POST.get("deletePost") and post.author == request.user:
             post.delete()
@@ -121,8 +122,9 @@ def post(request, slug):
     return render(request, 'posts/post.html', {
         'post': post,
         'form': form,
-        'other':other_posts,
-        'confirms': confirms
+        'other': other_posts,
+        'confirms': confirms,
+        'post_bids': bids
     }, context_instance=RequestContext(request))
 
 
