d7e6769582d228e06b53307ac9bd29a351221cb4
MalesSebastian <malessebastian@gmail.com>
2016-07-31 17:28:08 +0300

    Contact us validation

--- spartan/contact_us/forms.py
+++ spartan/contact_us/forms.py
@@ -28,10 +28,25 @@ class CreateContact(forms.ModelForm):
         model = ContactUs
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput({'required': 'required',
-                                     'placeholder': 'Name'}),
             'email': forms.EmailInput({'required': 'required',
                                        'placeholder': 'Email'}),
             'message': forms.Textarea(attrs={'required': 'required',
                                              'placeholder': 'Message'})
         }
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        if not first_name.isalpha():
+            raise forms.ValidationError("Enter a valid first name")
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data['last_name']
+        if not last_name.isalpha():
+            raise forms.ValidationError("Enter a valid last name")
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        if len(message) < 50:
+            raise forms.ValidationError(
+                "Your contact message has to be at least "
+                "50 characters long")
